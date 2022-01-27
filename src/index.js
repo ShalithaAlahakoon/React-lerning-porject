1e201a648fd5b23845cd628a64d5798479e7ca94
ShalithAlahakoon <shalitha97068@gmail.com>
2022-01-27 09:31:54 +0530

    react state with hooks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React ,{ useState ,useEffect}from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+function App(){
+  const[year,setYear] = useState(2050);
+  const[manager, setManager ] = useState("Alex");
+  const [status,setStatus] = useState("Open")
+  let button;
+  let nextManager;
+  if (status ==="Open") {
+    button = <button onClick={() => setStatus("Close")}>Close</button>
+  }else if (status ==="Close"){
+    button = <button onClick={() => setStatus("Open")}>Open</button>
+  }else{
+    button = <button onClick={() => setStatus("Open")}>Open</button>
+  }
+
+  if(manager ==="Alex"){
+    nextManager =<button onClick={() => setManager("Rachel")}>Change Manager</button>;
+  }else{
+    nextManager =<button onClick={() => setManager("Alex")}>Change Manager</button>;
+  }
+  
+  return(
+    <>
+      <div>
+        <h1>Year : {year}</h1>
+        <button onClick={() => setYear(year + 1)}>New year</button>
+      </div>
+      <div>
+        <h1>Manager on duty:{manager}</h1>
+        {nextManager}
+      </div>
+      <div>
+        <h1>Status : {status}</h1>
+        {button}
+        <button onClick={() => setStatus("Back in 5 min")}>Break</button>
+      </div>
+    </>
+    
+  );
+
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
